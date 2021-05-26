956d0e541297f87fe204f1c3d7a0ef9ad11c98eb
Melissa Heredia <herediamelissa19@gmail.com>
2021-05-26 19:03:46 -0400

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+//import react and react dom libraries
+import React from  'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//create a react componenets
+function getButtonText(){
+  return 'Click on me';
+}
+
+const App = () => {
+  return (
+    <div>
+   <label className="label" htmlFor= "name">Enter name</label>
+   <input name="name" type="text"/>
+   <button style={{backgroundColor:'blue', color:'white'}}>
+   {getButtonText()}
+   </button>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+//Take the react componenet and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
